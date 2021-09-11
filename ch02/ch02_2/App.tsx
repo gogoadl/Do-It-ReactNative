c123b5f3d7fd95e7e40c29af47690ac8016534ba
HyeonWoo Park <gogoadl@naver.com>
2021-09-11 18:59:08 +0900

    ch02_3

--- ch02/ch02_2/App.tsx
+++ ch02/ch02_2/App.tsx
@@ -2,9 +2,12 @@ import React from "react"
 import {SafeAreaView, Text} from "react-native"
 
 export default function App() {
+  const children = new Array(100)
+    .fill(null)
+    .map((notUsed, index) => <Text>Hello World! {index}</Text>)
   return (
     <SafeAreaView>
-      <Text>Hello JSX World!</Text>
+      {children}
     </SafeAreaView>
   )
 }
