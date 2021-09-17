4815023f90025b5c63b4f71077638cd022ef74de
HyeonWoo Park <gogoadl@naver.com>
2021-09-17 14:23:39 +0900

    ch04_3
    객체를 상태로 했을 때의 구현 방법 부터 하면됨

---
+++ ch04/ch04_3/App.tsx
@@ -0,0 +1,52 @@
+import React, {useMemo} from "react";
+import type {FC} from 'react'
+import { SafeAreaView, StyleSheet, FlatList, View, ScrollView, Dimensions, Text } from "react-native";
+import { Colors } from "react-native-paper";
+import PersonUsingValueState from './src/screens/PersonUsingValueState'
+import PersonUsingPassingState from './src/screens/PersonUsingPassingState'
+import PersonUsingObjectState from './src/screens/PersonUsingObjectState'
+import * as D from './src/data'
+
+const {width} = Dimensions.get('window')
+
+type PersonInformation = {
+  title: string
+  Component: FC<any>
+}
+
+const personInformations: PersonInformation[] = [
+  {title: 'PersonUsingValueState', Component: PersonUsingValueState},
+  {title: 'PersonUsingPassingState', Component: PersonUsingPassingState},
+  {title: 'PersonUsingObjectState', Component: PersonUsingObjectState}
+]
+
+const numberOfComponents = personInformations.length
+
+export default function App() {
+  const people = useMemo(() => D.makeArray(10).map(D.createRandomPerson), [])
+  const children = useMemo(() => 
+  personInformations.map(({title, Component}:personInformations) =>(
+    <View style={{flex: 1}} key={title}>
+      <Text style={[styles.text]}>{title}</Text>
+      <FlatList data={people}
+      renderItem={({item}) => <Component person={item} />}
+      keyExtractor={(item,index) => item.id}
+      ItemSeparatorComponent={() => <View style={styles.itemSeparator} />} />
+    </View>))
+    , [])
+
+    return (
+      <SafeAreaView style={styles.flex}>
+        <ScrollView horizontal contentContainerStyle={styles.horizontalScrollView}>
+          {children}
+        </ScrollView>
+      </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+  flex: {flex: 1},
+  itemSeparator: {borderWidth: 1, borderColor: Colors.grey500},
+  horizontalScrollView: {width: width * numberOfComponents},
+  text: {fontSize: 24, textAlign: 'center'}
+})
