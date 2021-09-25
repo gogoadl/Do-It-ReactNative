0fa4be76893dba89241cb1db28a3e1ec02b89dce
HyeonWoo Park <gogoadl@naver.com>
2021-09-25 18:20:29 +0900

    ch04_4

--- ch04/ch04_4/src/screens/Country.tsx
+++ ch04/ch04_4/src/screens/Country.tsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
-import {Colors} from 'react-native-paper'
-import * as D from '../data'
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import type {FC} from 'react';
+import * as D from '../data';
 
-const title = 'CopyMe'
-export default function CopyMe() {
+export type CountryProps = {
+  country: D.ICountry;
+};
+
+const Country: FC<CountryProps> = ({country}) => {
+  const {name, capital, population, subregion, region} = country;
   return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+    <View style={styles.view}>
+      <View>
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View>
+        <Text>capital: {capital}</Text>
+        <Text>population: {population}</Text>
+        <Text>subregion: {subregion}</Text>
+        <Text>region: {region}</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
+
+export default Country;
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
-})
+  view: {padding: 5},
+  name: {fontSize: 30, fontWeight: '400'},
+});
