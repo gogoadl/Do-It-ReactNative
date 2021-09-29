9584823c873c5cb31276637a838916277dfeee5a
HyeonWoo Park <gogoadl@naver.com>
2021-09-29 23:55:49 +0900

    ch05_1
    custom provider 만드는게 중요해보임

--- ch05/ch05_1/App.tsx
+++ ch05/ch05_1/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {DefaultTheme, DarkTheme} from 'react-native-paper';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import MainNavator from './src/screens/MainNavigator';
+import {ToggleThemeProvider} from './src/contexts';
 
 export default function App() {
+  const scheme = useColorScheme(); // 현재 모바일 기기의 운영체제가 다크모드인지, 라이트모드인지 얻어낸다
+  const [theme, setTheme] = useState(
+    scheme == 'dark' ? DarkTheme : DefaultTheme,
+  );
+  const toggleTheme = useCallback(
+    () => setTheme(theme => (theme.dark ? DefaultTheme : DarkTheme)),
+    [],
+  );
   return (
-    <SafeAreaView style={[styles.safeAreaView]}>
-      <MainNavator />
-    </SafeAreaView>
+    <AppearanceProvider>
+      <PaperProvider theme={DarkTheme}>
+        <ToggleThemeProvider toggleTheme={toggleTheme}>
+          <SafeAreaView style={[styles.safeAreaView]}>
+            <MainNavator />
+          </SafeAreaView>
+        </ToggleThemeProvider>
+      </PaperProvider>
+    </AppearanceProvider>
   );
 }
 
---
+++ ch05/ch05_1/src/contexts/ToggleThemeContext.tsx
@@ -0,0 +1,36 @@
+import React, {createContext, useContext} from 'react';
+
+export type ToggleThemeContextType = {
+  toggleTheme: () => void;
+}; // 공유하려는 데이터 속성
+const defaultToggleThemeContext = {
+  toggleTheme: () => {},
+}; // 공유하려는 데이터 속성 초깃값
+
+// createContext로 생성한 객체는 Provider, Consumer 컴포넌트를 제공
+const ToggleThemeContext = createContext<ToggleThemeContextType>(
+  defaultToggleThemeContext,
+);
+
+type ToggleThemeContextProps = {
+  toggleTheme: () => void;
+};
+export const ToggleThemeProvider: FC<ToggleThemeContextProps> = ({
+  children,
+  toggleTheme,
+}) => {
+  const value = {
+    // value 속성의 타입은 제네릭 함수 createContext<T>(defaultValue)의 타입과 같아야한다.
+    toggleTheme,
+  };
+  return (
+    <ToggleThemeContext.Provider value={value}>
+      {children}
+    </ToggleThemeContext.Provider>
+  );
+};
+
+export const useToggleTheme = () => {
+  const value = useContext(ToggleThemeContext);
+  return value;
+};
