8c0734c6f34a32ce85951d73e8a44e04780b9b98
HyeonWoo Park <gogoadl@naver.com>
2021-10-12 23:44:24 +0900

    ch06_2

--- ch06/ch06_2/src/screens/MainNavigator.tsx
+++ ch06/ch06_2/src/screens/MainNavigator.tsx
@@ -1,15 +1,28 @@
-import React, {useState} from 'react'
-import {BottomNavigation} from 'react-native-paper'
-// import Home from './Home'
+import React, {useState} from 'react';
+import {BottomNavigation} from 'react-native-paper';
+import Transform from './Transform';
+import InsideLayout from './InsideLayout';
+import Arithmetic from './Arithmetic';
+import Carousel from './Carousel';
 
 export default function MainNavigator() {
-  const [index, setIndex] = useState<number>(0)
+  const [index, setIndex] = useState<number>(0);
   const [routes] = useState([
-    //{key: 'home', title: 'Home', icon: 'home'},
-  ])
+    {key: 'transform', title: 'Transform', icon: 'arrow-collapse-right'},
+    {
+      key: 'insideLayout',
+      title: 'InsideLayout',
+      icon: 'animation-play-outline',
+    },
+    {key: 'arithmetic', title: 'Arithmetic', icon: 'format-annotation-plus'},
+    {key: 'carousel', title: 'Carousel', icon: 'border-vertical'},
+  ]);
   const renderScene = BottomNavigation.SceneMap({
-    // home: Home
-  })
+    transform: Transform,
+    insideLayout: InsideLayout,
+    arithmetic: Arithmetic,
+    carousel: Carousel,
+  });
 
   return (
     <BottomNavigation
@@ -17,5 +30,5 @@ export default function MainNavigator() {
       onIndexChange={setIndex}
       renderScene={renderScene}
     />
-  )
+  );
 }
