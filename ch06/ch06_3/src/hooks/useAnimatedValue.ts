c22d61670f13beec72f79a4a5623822bd7dadbac
HyeonWoo Park <gogoadl@naver.com>
2021-10-17 15:42:05 +0900

    ch06_3
    Animated.spring 사용하여 bounce 보다 부드럽게 이동시키기

--- ch06/ch06_3/src/hooks/useAnimatedValue.ts
+++ ch06/ch06_3/src/hooks/useAnimatedValue.ts
@@ -1,8 +1,13 @@
-import {useRef} from 'react';
+import {useMemo, useRef} from 'react';
 import {Animated} from 'react-native';
 
-export const useAnimatedValue = (initValue: number = 0): Animated.Value => {
-  return useRef(new Animated.Value(initValue)).current;
+const makeArray = (length: number) => new Array(length).fill(null);
+
+export const useAnimatedValue = (length: number, initValue: number = 0) => {
+  return useMemo(
+    () => makeArray(length).map(notUsed => new Animated.Value(initValue)),
+    [],
+  );
 };
 // const animValue = useRef(new Animated.Value(0)).current ->
 // const animValue = useAnimatedValue(0)
