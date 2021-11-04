8caef1f9a847dbe3c05203b9141c23eaa66c8dfa
HyeonWoo Park <gogoadl@naver.com>
2021-11-04 23:44:49 +0900

    ch07_3
    여러 아이콘 이용하여 bottom-tabs에 표시

--- ch07/ch07_3/src/screens/MainNavigator.tsx
+++ ch07/ch07_3/src/screens/MainNavigator.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Colors} from 'react-native-paper';
+import AntIcon from 'react-native-vector-icons/AntDesign';
+import FontawesomeIcon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import Login from './Login';
 import SignUp from './SignUp';
 import HomeNavigator from './HomeNavigator';
 
+import type {RouteProp, ParamListBase} from '@react-navigation/native';
+type TabBarIconProps = {focused: boolean; color: string; size: number};
+
+const screenOptions = ({route}: {route: RouteProp<ParamListBase, string>}) => {
+  return {
+    tabBarIcon: ({focused, color, size}: TabBarIconProps) => {
+      const {name} = route;
+      switch (name) {
+        case 'Login':
+          return <AntIcon name="login" size={size} color={color} />;
+        case 'SignUp':
+          return <FontawesomeIcon name="sign-in" size={size} color={color} />;
+      }
+      return <Icon name="home" size={size} color={color} />;
+    },
+  };
+};
+
 const Tab = createBottomTabNavigator();
 
 export default function MainNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={screenOptions}>
       <Tab.Screen name="Login" component={Login} />
       <Tab.Screen name="SignUp" component={SignUp} />
       <Tab.Screen
